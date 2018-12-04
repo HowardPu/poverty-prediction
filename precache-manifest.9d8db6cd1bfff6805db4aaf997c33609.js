e61d71096c3fd63733645a772a25cbd2444fe7a8
ChangyuLi18 <changl28@uw.edu>
2018-12-03 20:20:14 -0800

    Updates

---
+++ precache-manifest.9d8db6cd1bfff6805db4aaf997c33609.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c6048487a12a723a75eb25221a12b8b",
+    "url": "/poverty-prediction/static/media/poverty_child.0c604848.jpg"
+  },
+  {
+    "revision": "850a9bac9df9f29a01af",
+    "url": "/poverty-prediction/static/js/runtime~main.850a9bac.js"
+  },
+  {
+    "revision": "9d060fe742c1dcb06baa",
+    "url": "/poverty-prediction/static/js/main.9d060fe7.chunk.js"
+  },
+  {
+    "revision": "62b3c4d6900302480b92",
+    "url": "/poverty-prediction/static/js/1.62b3c4d6.chunk.js"
+  },
+  {
+    "revision": "9d060fe742c1dcb06baa",
+    "url": "/poverty-prediction/static/css/main.5496eb93.chunk.css"
+  },
+  {
+    "revision": "62b3c4d6900302480b92",
+    "url": "/poverty-prediction/static/css/1.8c5dd234.chunk.css"
+  },
+  {
+    "revision": "2c716f3f08ffa85d3120e40e9b120397",
+    "url": "/poverty-prediction/index.html"
+  }
+];
